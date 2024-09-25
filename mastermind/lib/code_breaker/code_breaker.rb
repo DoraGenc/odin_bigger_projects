52f0db26339223461ccd0c873e0c6ed3ae00dda6
Dora <dora.genc@ivx.com>
2024-09-25 13:30:10 +0200

    Saving it, before removing @winning_code

--- mastermind/lib/code_breaker/code_breaker.rb
+++ mastermind/lib/code_breaker/code_breaker.rb
@@ -1,332 +1,120 @@
-require_relative 'color_check_feedback.rb'
-require_relative 'color_check.rb'
-require_relative 'find_position_feedback.rb'
-require_relative 'find_position.rb'
-require_relative 'side_guess.rb'
-require_relative 'split_guess.rb'
-require_relative 'split_guess_feedback.rb'
-require_relative 'try_variations.rb'
 
 
 class CodeBreaker
 
   def initialize
 
-    @colors = (0..5).to_a
+    @S = (0..5).to_a.repeated_permutation(4).to_a
 
+    @possible_codes = @S.dup
     @feedback = nil
-    @last_feedback = nil
 
-    @strat_state = nil
+    @is_first_guess = true
+    @first_guess = [0, 0, 1, 1]
 
-    # strat_states (strategies)
-    @split_guess_strat = false
-    @split_guess_feedback = false
-
-    @color_check_strat = false
-    @color_check_feedback = false
-
-    @find_side_strat = false
-    @find_side_feedback = false
-
-    @find_position_strat = false
-    @find_position_feedback = false
-
-    @try_variations_strat = false
-
-    @current_guess = nil
     @last_guess = nil
-    @last_feedback = nil
-    @color_check_guess = nil
-
-    @colors_to_check = @colors.dup
-    @checked_colors = [] #checked Farben, deren state aber unklar sind
-
-    @included_colors = []
-    @excluded_colors = [] 
-
-    @undetermined_side_colors = []
-
-    @left_colors = []
-    @right_colors = []
-
-    # falls der nächste guess zum win führt:
-
-    @next_guess = nil
-    @win_next_round = false
+    @winning_code = nil
   end 
 
-  def receive_new_feedback(new_feedback)
 
-    self.feedback = new_feedback #1. feedback aktualisieren
-  end
+  # guess entscheidet zuerst, wie vorgegangen wird
 
-  def return_guess
+  def guess(counted_blows_and_hits)
 
-    puts "Return guess method called."
+    unless is_first_guess
+      puts "\nfeedback before: #{@feedback.inspect}"
+      get_feedback(counted_blows_and_hits)
+      puts "feedback after: #{@feedback.inspect}"
+    end 
 
-   new_guess = guess # 1. guess aufrufen
-   current_guess = new_guess
+    @last_guess = create_guess
+    puts "possible codes length = #{possible_codes.length}"
 
-   update_last_feedback
-   update_last_guess
-
-   return new_guess # 2. guess returnen
- end
-
-  def change_strat(strat)
-
-    puts "STRAT BEFORE CHANGE: #{strat_state}"
-    self.strat_state = strat
-
-    puts "STRAT AFTER CHANGE: #{strat_state}"
+    return @last_guess
   end 
 
-  def first_guess
+  def create_guess
 
-    first_guess = [colors_to_check[0], colors_to_check[0], colors_to_check[1], colors_to_check[1]]
+    if is_first_guess
 
-    change_color_state(colors_to_check[0])
-    change_color_state(colors_to_check[0])
+      puts "it's the first guess!"
 
-    return first_guess
-  end
+      @is_first_guess = false #hier ist @ nötig,
+      guess = first_guess #aber hier nicht?!
 
-  def change_color_state(color)
+    else 
+      mimic
 
-    colors_to_check.delete(color)
-    checked_colors << color
-  end
-
-  def update_last_feedback
-
-    self.last_feedback = feedback
-    self.feedback = nil
-  end
-
-  def update_last_guess
-
-    last_guess = current_guess
-    current_guess = nil
-  end
-
-
-  def update_excluded_colors!(colors) #nimmt nur index zahlen an 
-
-    if colors.to_s.length == 1
-      
-      self.excluded_colors << colors.to_i
-
-    elsif colors.to_s.length > 1
-
-      self.excluded_colors << colors
-      self.excluded_colors = excluded_color.flatten!.uniq!
-    end
-  end
-
-  def update_included_colors!(colors)
-
-    self.included_colors << colors
-    self.included_colors = excluded_color.flatten!
-  end
-
-  def update_quantity!(hash)
-    # Das Hash ist: color_index => quantity
-  
-    colors.each do |color|
-      if hash.key?(color)
-        quantity = hash[color]
-  
-        quantity.times do
-          update_included_colors!(color)
-        end
-      end
-    end
-  end
-
-
-  def guess
-
-     puts "Guess method called. Current strat_state: #{@strat_state.inspect}"
-     puts caller.join("\n")  # Zeigt den Aufruf-Stack an
-
-    created_guess = false
-
-    if win_next_round
-      
-      created_guess = true
-      return next_guess
-    end
-
-    case self.strat_state
-    when nil
-        
-      pp "FIRST GUESS CASE! The strat state is: #{strat_state.inspect}"
-
-      new_guess = first_guess
-
-      change_strat(split_guess_feedback)
-      feedback = 0
-      return guess
-
-    when split_guess_strat
-
-      puts "The strat state is: #{strat_state.inspect}"
-
-      split_guess_strat = false
-      puts strat_state
-
-      split_guess = SplitGuess.new
-      guess = split_guess.guess(colors_to_check)
-      change_strat(split_guess_feedback)
-
-      return guess
-
-    when split_guess_feedback
-
-      puts "The strat state is: #{strat_state.inspect}"
-      split_guess_feedback = false
-
-      split_feedback = SplitGuessFeedback.new(feedback, last_guess)
-      split_feedback.evaluate
-      
-      if split_feedback.split_guess
-
-        win_next_round = true
-        next_guess = split_feedback.guess
-
-      elsif split_feedback.change_strat
-        change_strat(color_check_strat)
-
-      elsif split_feedback.excluded_colors
-        update_excluded_colors!(split_feedback.excluded_colors)
-      end
-
-    
-    when color_check_strat
-
-      puts "The strat state is: #{strat_state.inspect}"
-
-      color_check_strat = false
-      puts strat_state
-
-      color_check = ColorCheck.new
-      guess = color_check.guess(last_guess)
-      change_strat(color_check_feedback)
-
-      return guess
-
-    when color_check_feedback
-
-      puts "The strat state is: #{strat_state.inspect}"
-
-      color_check_feedback = false
-      puts strat_state
-
-      color_check_feedback = ColorCheckFeedback.new(feedback, last_guess, last_feedback, color_check_guess)
-      color_check_feedback.evaluate
-
-      update_included_colors!(color_check.included_colors)
-      update_excluded_colors!(color_check.excluded_colors)
-      update_quantity!(color_check.quantity)
-
-      if colors_to_check.empty?
-        change_strat(find_position_strat)
-
+      unless winning_code == nil
+        guess = winning_code
       else 
-        change_strat(split_guess_strat)
-      end
-    
-      
-    when find_position_strat
+        guess = possible_codes.sample
+     end
 
-      puts "The strat state is: #{strat_state.inspect}"
-
-      find_position_strat = false
-      puts strat_state
-
-      find_position_guess = FindPosition.new
-
-      undetermined_side_colors.delete_at(0)
-      guess = find_position_guess.guess(undetermined_side_colors, excluded_colors)
-      change_strat(find_position_feedback)
-
-      return guess
-
-  
-    when find_position_feedback
-
-      puts "The strat state is: #{strat_state.inspect}"
-
-      find_position_feedback = false
-      puts strat_state
-
-      find_position_feedback = FindPositionFeedback.new
-      find_position_feedback.evaluate(feedback, last_guess)
-
-      if find_position_feedback.left_color
-        undetermined_side_colors.delete_at(find_position_feedback.left_color)
-        left_colors << find_position_feedback.left_color
-
-      else
-        undetermined_side_colors.delete_at(find_position_feedback.right_color)
-        right_colors << find_position_feedback.right_color
-      end
-
-      change_strat(find_position_guess)
-
-      if undetermined_side_colors.length == 1
-        determine_remaining_side
-        change_strat(try_variations_strat)
-      end 
-
-
-    when try_variations_strat #wenn wir nur noch left & right colors haben :3
-
-      puts "The strat state is: #{strat_state.inspect}"
-      
-      try_variations_strat = false
-      puts strat_state
-
-      try_variations_guess = TryVariations.new
-      next_guess = try_variations_guess.next_guess
-      guess = try_variations_guess.guess
-
-      win_next_round = true
-
-      return guess
-
-    else
-      puts "No case found????"
-
-    end
-  
+    end 
     return guess
-  end
+  end 
 
 
-  def determine_remaining_side
+  # mimic 
 
-    if left_colors.length == 2
-      left_colors << undetermined_side_colors.first #first, da es undetermined_side_colors ein Array ist
+  def mimic
 
-    elsif right_colors.length < 2
-      right_colors << undetermined_side_color.first
+    possible_codes.each do |possible_code|
+
+      mimicked_feedback = mimic_feedback(possible_code)
     end
   end
+
+
+  def mimic_feedback(code)
+
+    blows = 0
+    hits = 0 
+
+    color_count = Hash.new(0)
+    last_guess.each { |color| color_count[color] += 1 }
+
+
+    code.each_with_index do |number, index|
   
+      if last_guess[index] == number
+        hits += 1
+        color_count[number] -= 1
+      end 
+    end 
 
+    code.each_with_index do |number, index|
+
+      if last_guess.include?(number) && color_count[number] > 0
+        blows += 1
+        color_count[number] -= 1
+      end
+    end 
+
+    mimicked_feedback = [blows, hits]
+
+    if mimicked_feedback != @feedback
+      laser_beam(code)
+
+    elsif mimicked_feedback == [4, 0]
+      @winning_code = code
+    end
+
+    mimicked_feedback
+  end
+
+  def laser_beam(impossible_code)
+    possible_codes.delete(impossible_code)
+  end
   
-
-
+  def get_feedback(blows_and_hits)
+    @feedback = blows_and_hits #geht nicht mit nur "feedback"????
+  end
 
 
   private
 
-  attr_accessor :feedback, :split_guess_strat, :excluded_colors, :included_colors, :last_feedback, :last_guess, 
-              :split_guess_feedback, :color_check_strat, :color_check_feedback, :find_side_strat, :find_side_feedback, 
-              :find_position_strat, :find_position_feedback, :try_variations_strat, :colors_to_check, :checked_colors, 
-              :undetermined_side_colors, :left_colors, :right_colors, :next_guess, :win_next_round, :strat_state, :current_guess
-
-  attr_reader :colors
-end
+  attr_accessor :feedback, :possible_codes, :is_first_guess, :last_guess, :winning_code
+  attr_reader :first_guess
+end 
