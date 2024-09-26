fbc3c5213375f458d85c8dd62c42446461803184
Dora <dora.genc@ivx.com>
2024-09-26 11:51:39 +0200

    Simplify the structure

--- mastermind/lib/board.rb
+++ mastermind/lib/board.rb
@@ -1,6 +1,12 @@
 require_relative 'feedback'
 
 class Board
+
+  HEADER = '|   Blows   |   Hits    |    1    |    2    |    3    |    4    |'
+  HORIZONTAL_SEPERATOR = '-'
+  VERTICAL_SEPERATOR = '|'
+  COLUMN_WIDTH = 9
+
   def initialize
     @current_board = INITIAL_BOARD
     @blows_and_hits = Array.new(12) { %w[x x] }
@@ -9,21 +15,27 @@ class Board
   def add_to_board!(guess, current_blows_and_hits, guess_counter)
     
     index = guess_counter
-    current_board[index] = guess
 
+    current_board[index] = guess
     blows_and_hits[index] = current_blows_and_hits
   end
 
   def print_board(_guess_counter)
-    header = '|   Blows   |   Hits    |    1    |    2    |    3    |    4    |'
-    horizontal_separator = '-'
-    vertical_separator = '|'
-    column_width = 9
+    
+    print_header
+    print_rows
+  end
+
+
+  def print_header
 
     puts "\n"
     puts "BOARD:\n"
-    puts header
-    puts horizontal_separator * header.length
+    puts HEADER
+    puts HORIZONTAL_SEPERATOR * HEADER.length
+  end
+
+  def print_rows
 
     current_board.each_with_index.reverse_each do |row, row_index| #das reverse.each sorgt dafür, dass zuerst die letzte Zeile des Boards durchlaufen wird (mit 'x')
       print "|     #{blows_and_hits[row_index][0]}     |     #{blows_and_hits[row_index][1]}     "
@@ -31,18 +43,19 @@ class Board
       row.each do |guess|
         guess = guess.to_s
 
-        empty_space = column_width - guess.length
+        empty_space = COLUMN_WIDTH - guess.length
         left_padding = empty_space / 2
         right_padding = empty_space - left_padding
 
         # Einzelne Spalten
-        print "#{vertical_separator}#{' ' * left_padding}#{guess}#{' ' * right_padding}"
+        print "#{VERTICAL_SEPERATOR}#{' ' * left_padding}#{guess}#{' ' * right_padding}"
       end
-      print vertical_separator
+      print VERTICAL_SEPERATOR
       puts "\n"
-      puts horizontal_separator * header.length
+      puts HORIZONTAL_SEPERATOR * HEADER.length
     end
   end
+
 
   private
 
