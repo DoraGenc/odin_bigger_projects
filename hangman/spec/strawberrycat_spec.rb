daa25a591ca5a477fff74d04c53d611dcb67fc4a
Dora <dora.genc@ivx.com>
2024-10-25 14:54:45 +0200

    Can meow, take damage, change its mood and print

--- hangman/spec/strawberrycat_spec.rb
+++ hangman/spec/strawberrycat_spec.rb
@@ -49,6 +49,72 @@ RSpec.describe StrawberryCat do
         expect(cat.mood).to eq(1)
       end
     end
+
+    context "when the argument is a float" do
+
+      it "is converted to an integer" do
+        cat.change_mood(1.2)
+        expect(cat.mood).to eq(1)
+      end
+    end
   end
 
+  describe "#meow" do
+    context "when the cat's mood is below 0" do
+      it "prints an angry meow" do
+        cat.change_mood(-1)
+        expect { cat.meow }.to output("\n[\e[0;31;49mLord of Mischief\e[0m] \"Strawberry Cat\": meoww :<").to_stdout
+      end
+    end
+
+    context "when the cat's mood is happy" do
+      it "prints a happy meow" do
+        cat.change_mood(2)
+        expect { cat.meow }.to output("\n[\e[0;31;49mLord of Mischief\e[0m] \"Strawberry Cat\": meowwww.. :3").to_stdout
+      end
+    end
+
+    context "when the cat's mood is neutral" do
+      it "prints a neutral meow" do
+        expect { cat.meow }.to output("\n[\e[0;31;49mLord of Mischief\e[0m] \"Strawberry Cat\": purr... :3").to_stdout
+      end
+    end
+  end
+
+  describe "#take_damage" do
+    context "when taking damage" do
+      it "looses exactly 1 hp" do
+        cat.take_damage!
+        expect(cat.hp).to eq(6)
+      end
+
+      it "looses one mood point" do
+        cat.take_damage!
+        expect(cat.mood).to eq(-1)
+      end
+    end
+  end
+
+  describe "#print_cat" do
+
+    before do
+      allow(cat).to receive(:print_pieces)
+      allow(cat).to receive(:print_cat).and_call_original
+    end
+
+    context "when calling print_cat before taking damage" do
+      it "prints the whole cat" do
+        cat.print_cat
+        expect(cat).to have_received(:print_pieces).with(1)
+      end
+    end
+
+    context "when calling print_cat after taking damage" do
+      it "does not print the whole cat" do
+        cat.take_damage!
+        cat.print_cat
+        expect(cat).to have_received(:print_pieces).with(2)
+      end
+    end
+  end
 end
