616a62971ae73a5d76a848dbd81f289d0467d5de
Dora <dora.genc@ivx.com>
2024-12-06 16:16:17 +0100

    WIP

--- binary_search_trees/spec/tree_spec.rb
+++ binary_search_trees/spec/tree_spec.rb
@@ -250,4 +250,64 @@ RSpec.describe Tree do
       end
     end
   end
+
+  describe "#delete" do
+    let(:tree) { described_class.new([1, 2, 3, 4, 5, 6, 7, 8, 9]) }
+
+    context "when the node to delete is the root" do
+      it "deletes the root" do
+        tree.delete(5)
+
+        expect(tree.root).to eq(nil)
+      end
+    end
+
+    context "when the node to delete is a leaf" do
+      it "deletes the leaf" do
+        tree.delete(1)
+
+        expect(tree.node_exists?(1)).to eq(false)
+      end
+    end
+
+    context "when the node to delete has only one child" do
+      it "deletes the node" do
+        tree.delete(8)
+
+        expect(tree.node_exists?(8)).to eq(false)
+      end
+
+      it "does not delete its children" do
+        tree.delete(2)
+
+        expect(tree.node_exists?(1)).to eq(true)
+      end
+
+      it "links its children to the correct node" do
+        tree.delete(2)
+        orphaned_child_value = 1
+
+        expect(tree.root.left_children.left_children.value).to eq(1)
+      end
+    end
+
+    context "when the node to delete has 2 children" do
+      it "deletes the node" do
+        tree.delete(3)
+
+        expect(tree.node_exists?(3)).to eq(false)
+      end
+
+      it "does not delete its children" do
+        #tree.pretty_print
+        tree.delete(3)
+        puts ""
+        tree.pretty_print
+
+        expect(tree.node_exists?(4)).to eq(true)
+        expect(tree.node_exists?(2)).to eq(true)
+        expect(tree.node_exists?(1)).to eq(true)
+      end
+    end
+  end
 end
