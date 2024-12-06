f053c63b3e4241acc47ea1d52ec0200ee1da9258
Dora <dora.genc@ivx.com>
2024-12-06 10:03:35 +0100

    WIP

--- binary_search_trees/spec/tree_spec.rb
+++ binary_search_trees/spec/tree_spec.rb
@@ -3,7 +3,6 @@ require_relative "../lib/tree.rb"
 RSpec.describe Tree do
 
   describe "#build_tree" do
-
     context "when building a small tree" do
       it "returns nil if the array is empty or the argument is nil (base case)" do
         sorted_array = []
@@ -97,6 +96,122 @@ RSpec.describe Tree do
         expect(tree.root).not_to eq(nil)
         expect(tree.root.left_children).not_to eq(nil)
         expect(tree.root.right_children).not_to eq(nil)
+      end
+    end
+
+    context "when binary_tree_values is empty" do
+      it "assigns the given array to binary_tree_values" do
+        tree = Tree.new
+        tree.build_tree([1])
+        expect(tree.binary_tree_values).to eq([1])
+      end
+    end
+  end
+
+  describe "#find" do
+    context "when a tree exists" do
+      let(:tree) { described_class.new([1, 2, 3, 4, 5, 6, 7, 8, 9])}
+
+      context "when a value exists" do
+        xit "returns the node with the value searched for" do
+          expect(tree.find(5)).to eq(tree.root)
+        end
+      end
+    end
+  end
+
+  describe "#find_node" do
+    context "when a tree exists" do
+      let(:tree) { described_class.new([1, 2, 3])}
+
+      context "when the value is invalid" do
+        it "returns an error" do
+          invalid_value = -1
+          expect(tree.find_node(invalid_value)).to eq("Invalid value. Please only choose positive Integers or Floats.")
+        end
+      end
+
+      context "when the value is valid" do
+        it "returns the correct node" do
+          searched_value = 2.5
+          returned_node = tree.find_node(searched_value)
+
+          expect(returned_node.value).to eq(3)
+        end
+      end
+
+      context "when the node to return would be the root" do
+        it "returns the root" do
+          searched_value = 2
+          returned_node = tree.find_node(searched_value)
+
+          expect(returned_node).to eq(tree.root)
+        end
+      end
+
+      context "when the root is nil" do
+        it "returns nil" do
+          empty_tree = Tree.new
+          non_existent_value = 1
+          expect(empty_tree.find_node(non_existent_value)).to eq(nil)
+        end
+      end
+
+      context "when the tree is larger" do
+        it "returns the correct node" do
+          large_tree = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+          searched_value = 10
+          returned_node =  tree.find_node(searched_value)
+          correct_returned_node_value = 9
+
+          expect(returned_node.value).to eq(correct_returned_node_value)
+        end
+      end
+    end
+  end
+
+  describe "#insert" do
+    let(:tree) { described_class.new([1, 2, 3, 4, 5, 6, 7, 8, 9]) }
+
+    context "when the value is invalid" do
+      context "when the value is nil" do
+        it "returns an error" do
+          invalid_value = nil
+          expect(tree.insert(invalid_value)).to eq("Invalid value. Please only choose positive Integers or Floats.")
+        end
+      end
+
+      context "when the value is smaller than 0" do
+        it "returns an error" do
+          invalid_value = -1
+          expect(tree.insert(invalid_value)).to eq("Invalid value. Please only choose positive Integers or Floats.")
+        end
+      end
+    end
+
+    context "when the value is valid" do
+      context "when the value is an Integer" do
+        it "does not return an error" do
+          valid_value = 1
+          expect(tree.insert(valid_value)).not_to eq("Invalid value. Please only choose positive Integers or Floats.")
+        end
+      end
+
+      context "when the value is a Float" do
+        it "does not return an error" do
+          valid_value = 1.5
+          expect(tree.insert(valid_value)).not_to eq("Invalid value. Please only choose positive Integers or Floats.")
+        end
+      end
+
+      context "when adding a node to a leaf node (no children)" do
+        xit "adds a child to the right position" do
+          new_tree = Tree.new([1, 2, 3])
+          new_tree.build_tree
+          new_tree.insert(2.5)
+
+          expect(new_tree.root.right_children.right_children.value).to eq(2.5)
+        end
       end
     end
   end
